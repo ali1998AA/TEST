d65b2286c7ada697a9735fa5b9662d2463495b1c
ali1998AA <alinadhum19@gmail.com>
2023-06-20 17:40:52 +0300

    last update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from './pages/home';
+import Css from "./pages/css";
+import Html from "./pages/html";
+import Js from "./pages/javascript";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <p>oops page is not found ......</p>
+  },
+
+  {
+    path: "/css",
+    element: <Css />,
+  },
+
+  {
+    path: "/html",
+    element: <Html />,
+  },
+
+  {
+    path: "/js",
+    element: <Js />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
